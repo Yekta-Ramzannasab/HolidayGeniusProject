8e1137beb86f6b68b26b12de6abd92ab9d47ece8
Yekta <yektaramzannasab@gmail.com>
2025-04-21 19:18:19 +0330

    Delete src/main/java directory

--- src/main/java/roles/Account.java
+++
@@ -1,8 +0,0 @@
-package roles;
-
-public interface Account {
-    public void register();
-    public void infoWriter(String filename, String username, String password, String name, String email);
-    public void usernameWriter(String filename, String username);
-
-}
